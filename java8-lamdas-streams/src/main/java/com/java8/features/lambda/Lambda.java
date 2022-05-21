d51feeba3770dfe4f501151ed39467ae8c38fe76
Ansuman Mohanty <ansumanm20@gmail.com>
2022-05-21 12:55:18 +0530

    lambdas
    lambdas

--- java8-lamdas-streams/src/main/java/com/java8/features/lambda/Lambda.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/lambda/Lambda.java
@@ -2,7 +2,12 @@ package com.java8.features.lambda;
 
 public class Lambda {
     public static void main(String[] args) {
-
+        Demo demo = str -> str.length();
+        System.out.println(demo.stringLength("hiii"));
     }
 }
 
+@FunctionalInterface
+interface Demo{
+    public int stringLength(String str);
+}
