a6bf22c6d979bd994fc3dabf08d925b037d41baa
Ansuman Mohanty <ansumanm20@gmail.com>
2022-05-23 14:20:01 +0530

    static method interface
    static method interface

--- java8-lamdas-streams/src/main/java/com/java8/features/java8Interface/StaticMethodInterface.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/java8Interface/StaticMethodInterface.java
@@ -1,4 +1,12 @@
 package com.java8.features.java8Interface;
 
 public class StaticMethodInterface {
+    public static void main(String[] args) {
+        Inter.m1();
+    }
+}
+interface Inter{
+    public static void m1(){
+        System.out.println("static method");
+    }
 }
---
+++ java8-lamdas-streams/src/main/java/com/java8/features/java8Interface/Test.java
@@ -0,0 +1,7 @@
+package com.java8.features.java8Interface;
+
+public interface Test {
+    public static void main(String[] args) {
+        System.out.println("without class java program");
+    }
+}
