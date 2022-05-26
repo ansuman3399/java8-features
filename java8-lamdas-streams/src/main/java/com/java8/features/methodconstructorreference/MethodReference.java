74ae566469b5984f52368979aac051da4d3f569e
Ansuman Mohanty <ansumanm20@gmail.com>
2022-05-26 18:56:41 +0530

    method an constructor reference
    method an constructor reference

---
+++ java8-lamdas-streams/src/main/java/com/java8/features/methodconstructorreference/MethodReference.java
@@ -0,0 +1,14 @@
+package com.java8.features.methodconstructorreference;
+
+public class MethodReference {
+    static void m2(){
+        System.out.println("m2 reference");
+    }
+
+    public static void main(String[] args) {
+        3
+    }
+}
+interface ref{
+    void m1();
+}
