b3b2f5daa1d064efbbdb5843c3812cf077b85719
Ansuman Mohanty <ansumanm20@gmail.com>
2022-05-28 13:19:58 +0530

    ZonedDateTime
    ZonedDateTime

--- java8-lamdas-streams/src/main/java/com/java8/features/DateTimeAPI/ZoneId.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/DateTimeAPI/ZoneId.java
@@ -1,7 +1,13 @@
 package com.java8.features.DateTimeAPI;
 
+import java.time.*;
+
 public class ZoneId {
     public static void main(String[] args) {
+
+//        ZoneId zone = ZoneId.of("America/Los_Angeles");
+//        ZonedDateTime zd = ZonedDateTime.now(zone);
+//        System.out.println(zd);
         System.out.println(java.time.ZoneId.systemDefault());
     }
 }
