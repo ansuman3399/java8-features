393397f18682db2e6905df6a837cbdafeab0cf05
Ansuman Mohanty <ansumanm20@gmail.com>
2022-05-30 20:31:22 +0530

    predicate theory
    predicate theory

--- java8-lamdas-streams/src/main/java/com/java8/features/predicate/PredicateFunctionalInterface.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/predicate/PredicateFunctionalInterface.java
@@ -1,8 +1,11 @@
 package com.java8.features.predicate;
 
 //predicate-predefined functional interface
+//to check a particular conditional checking - java.util.function;
+//functional interface
+//only 1 method - boolean test(T t)
 public class PredicateFunctionalInterface {
     public static void main(String[] args) {
-        
+
     }
 }
