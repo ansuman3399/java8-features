9175d6b7e039f6d77a80da62d07a99d1ee2e535a
Ansuman Mohanty <ansumanm20@gmail.com>
2022-05-30 20:28:10 +0530

    some changes
    some changes

--- java8-lamdas-streams/src/main/java/com/java8/features/predicate/PredicateFunctionalInterface.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/predicate/PredicateFunctionalInterface.java
@@ -1,7 +1,8 @@
 package com.java8.features.predicate;
 
+//predicate-predefined functional interface
 public class PredicateFunctionalInterface {
     public static void main(String[] args) {
-
+        
     }
 }
