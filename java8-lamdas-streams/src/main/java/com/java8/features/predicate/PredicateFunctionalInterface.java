34b2ab1b2c098b59be0d51ae3e0c6f2f17a336b1
Ansuman Mohanty <ansumanm20@gmail.com>
2022-05-30 20:37:05 +0530

    predicate program
    predicate program

--- java8-lamdas-streams/src/main/java/com/java8/features/predicate/PredicateFunctionalInterface.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/predicate/PredicateFunctionalInterface.java
@@ -1,11 +1,14 @@
 package com.java8.features.predicate;
 
+import java.util.function.Predicate;
+
 //predicate-predefined functional interface
 //to check a particular conditional checking - java.util.function;
 //functional interface
 //only 1 method - boolean test(T t)
 public class PredicateFunctionalInterface {
     public static void main(String[] args) {
-
+        Predicate<Integer> predicate = i -> i > 10;
+        System.out.println(predicate.test(11));
     }
 }
