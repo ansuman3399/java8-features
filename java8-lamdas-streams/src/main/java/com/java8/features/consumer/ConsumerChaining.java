47d940233fa6c994c3115b8f4f6e8e00f6f05170
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-01 13:47:30 +0530

    Consumer chaining
    Consumer chaining

---
+++ java8-lamdas-streams/src/main/java/com/java8/features/consumer/ConsumerChaining.java
@@ -0,0 +1,16 @@
+package com.java8.features.consumer;
+
+public class ConsumerChaining {
+    public static void main(String[] args) {
+        
+    }
+}
+class Movie{
+    String name;
+    String result;
+
+    public Movie(String name,String result) {
+        this.name = name;
+        this.result=result;
+    }
+}
