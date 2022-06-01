d56b6d5ed14392fb108f0b94ce572f3f9b3bec98
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-01 15:27:41 +0530

    Update SupplierDemo.java
    SupplierDemo

--- java8-lamdas-streams/src/main/java/com/java8/features/supplier/SupplierDemo.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/supplier/SupplierDemo.java
@@ -1,7 +1,9 @@
 package com.java8.features.supplier;
-
+//no input
+//only returns R type
+//R get();
 public class SupplierDemo {
     public static void main(String[] args) {
-        
+
     }
 }
