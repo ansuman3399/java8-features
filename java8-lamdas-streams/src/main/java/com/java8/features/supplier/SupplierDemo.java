e4ffd6ca1604bae56d6218ec5f8c71198c37a64f
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-01 15:29:28 +0530

    Supplier demo
    Supplier demo

--- java8-lamdas-streams/src/main/java/com/java8/features/supplier/SupplierDemo.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/supplier/SupplierDemo.java
@@ -1,9 +1,15 @@
 package com.java8.features.supplier;
+
+import java.util.Date;
+import java.util.function.Supplier;
+
 //no input
 //only returns R type
 //R get();
+//no default or static method
 public class SupplierDemo {
     public static void main(String[] args) {
-
+        Supplier<Date> supplier = () -> new Date();
+        System.out.println(supplier.get());
     }
 }
