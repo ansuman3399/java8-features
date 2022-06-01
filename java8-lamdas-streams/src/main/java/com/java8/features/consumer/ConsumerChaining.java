26d091284f1ea8e89346d48be07579c72c38fe5c
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-01 15:23:54 +0530

    Consumer chaining
    Consumer chaining

--- java8-lamdas-streams/src/main/java/com/java8/features/consumer/ConsumerChaining.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/consumer/ConsumerChaining.java
@@ -1,8 +1,13 @@
 package com.java8.features.consumer;
 
+import java.util.function.Consumer;
+
 public class ConsumerChaining {
     public static void main(String[] args) {
-
+        Consumer<Movie> c1 = movie -> System.out.println("Movie is "+movie.name);
+        Consumer<Movie> c2 = movie -> System.out.println(movie.name+" movie is "+movie.result);
+        Consumer<Movie> c3 = movie -> System.out.println("Movie "+movie.name+ " information stored");
+        c1.andThen(c2).andThen(c3).accept(new Movie("Team A","Super hit"));
     }
 }
 class Movie{
---
+++ java8-lamdas-streams/src/main/java/com/java8/features/supplier/SupplierDemo.java
@@ -0,0 +1,7 @@
+package com.java8.features.supplier;
+
+public class SupplierDemo {
+    public static void main(String[] args) {
+        
+    }
+}
