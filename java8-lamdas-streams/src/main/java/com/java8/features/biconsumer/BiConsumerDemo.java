5b392180b8a4924efa41e32e561c9c38972c5935
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-02 20:04:48 +0530

    file rename
    file rename

--- java8-lamdas-streams/src/main/java/com/java8/features/biconsumer/BiConsumerDemo.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/biconsumer/BiConsumerDemo.java
@@ -2,7 +2,7 @@ package com.java8.features.biconsumer;
 
 import java.util.function.BiConsumer;
 
-public class BoConsumerDemo {
+public class BiConsumerDemo {
     public static void main(String[] args) {
         BiConsumer<String,String> biConsumer = (s1,s2)-> System.out.println(s1+" "+s2);
         biConsumer.accept("ansuman","mohanty");
