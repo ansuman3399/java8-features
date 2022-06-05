5f7f3e7e17de7d8c8c6138556ca159fe95c702fd
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-05 18:18:32 +0530

    FlatMapDemo
    FlatMapDemo

--- java8-lamdas-streams/src/main/java/com/java8/features/streams/FlatMapDemo.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/streams/FlatMapDemo.java
@@ -1,7 +1,20 @@
 package com.java8.features.streams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FlatMapDemo {
     public static void main(String[] args) {
-        
+        List<List<String>> list = new ArrayList<>();
+        list.add(Arrays.asList("list1a", "list2a", "list3a"));
+        list.add(Arrays.asList("list1b", "list2b", "list3b"));
+        list.add(Arrays.asList("list1c", "list2c", "list3c"));
+
+//        list.stream().map(s->s.get(0)).collect(Collectors.toList());
+//        list.stream().map(List::listIterator).collect(Collectors.toList());
+        List<String> collect = list.stream().flatMap(List::stream).map(s -> s.toUpperCase()).collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
