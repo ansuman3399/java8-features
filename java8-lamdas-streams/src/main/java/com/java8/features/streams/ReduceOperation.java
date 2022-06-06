c05b4b537eb0e00c9e396c9488df11eec5c37c89
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-06 11:05:48 +0530

    optional case
    optional case

--- java8-lamdas-streams/src/main/java/com/java8/features/streams/ReduceOperation.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/streams/ReduceOperation.java
@@ -2,6 +2,7 @@ package com.java8.features.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 //reduce the content of the stream
 public class ReduceOperation {
@@ -9,5 +10,8 @@ public class ReduceOperation {
         List<Integer> list = Arrays.asList(1, 2, 5, 7);
         Integer reduce = list.stream().reduce(1, (a, b) -> a * b);
         System.out.println(reduce);
+
+        Optional<Integer> reduce1 = list.stream().reduce((a, b) -> a * b);
+        System.out.println(reduce1.isPresent()?reduce1.get():null);
     }
 }
