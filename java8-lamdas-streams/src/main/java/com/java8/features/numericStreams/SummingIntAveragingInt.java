0c85141d3010c5174ae74774866d042a74126635
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-09 20:08:39 +0530

    SummingIntAveragingInt
    SummingIntAveragingInt

---
+++ java8-lamdas-streams/src/main/java/com/java8/features/numericStreams/SummingIntAveragingInt.java
@@ -0,0 +1,19 @@
+package com.java8.features.numericStreams;
+
+import com.java8.features.data.StudentDatabase;
+
+import java.util.stream.Collectors;
+
+public class SummingIntAveragingInt {
+    public static void main(String[] args) {
+        Integer collect = StudentDatabase.getAllStudents()
+                .stream()
+                .collect(Collectors.summingInt(StudentDatabase::getBooks));
+        System.out.println(collect);
+
+        Double collect1 = StudentDatabase.getAllStudents()
+                .stream()
+                .collect(Collectors.averagingInt(StudentDatabase::getBooks));
+        System.out.println(collect1);
+    }
+}
