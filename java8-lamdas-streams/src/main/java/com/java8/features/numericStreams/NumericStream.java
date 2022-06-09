12ea28dad7e74910a34b4e285880da9690469564
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-09 13:41:27 +0530

    numeric stream
    numeric stream

--- java8-lamdas-streams/src/main/java/com/java8/features/numericStreams/NumericStream.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/numericStreams/NumericStream.java
@@ -1,7 +1,12 @@
 package com.java8.features.numericStreams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class NumericStream {
     public static void main(String[] args) {
-        
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Integer sum = list.stream().reduce(0, (a, b) -> a + b);
+        System.out.println(sum);
     }
 }
