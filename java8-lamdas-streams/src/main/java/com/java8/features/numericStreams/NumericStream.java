8abf8ce20124df4832be0728e14864c10f720211
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-09 13:43:32 +0530

    Int stream
    Int stream

--- java8-lamdas-streams/src/main/java/com/java8/features/numericStreams/NumericStream.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/numericStreams/NumericStream.java
@@ -2,11 +2,15 @@ package com.java8.features.numericStreams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class NumericStream {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        Integer sum = list.stream().reduce(0, (a, b) -> a + b);
+        Integer sum = list.stream().reduce(0, (a, b) -> a + b); //unboxing to convert Integer to int and then to Integer
         System.out.println(sum);
+
+        int sum1 = IntStream.rangeClosed(1, 9).sum();
+        System.out.println(sum1);
     }
 }
