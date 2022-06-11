143860e06775cfb09494390d65859efd3ee466a5
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-11 18:08:03 +0530

    optional
    optional

---
+++ java8-lamdas-streams/src/main/java/com/java8/features/optional/OptionalDemo.java
@@ -0,0 +1,17 @@
+package com.java8.features.optional;
+
+import com.java8.features.data.StudentDatabase;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class OptionalDemo {
+    public static void main(String[] args) {
+        Optional<List<StudentDatabase>> collect = Optional.ofNullable(StudentDatabase.getAllStudents()
+                .stream()
+                .filter(studentDatabase -> studentDatabase.getBooks() > 10)
+                .collect(Collectors.toList()));
+        System.out.println(collect.isPresent()?collect.get():null);
+    }
+}
