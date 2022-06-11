bde5efdd9f8185c47dfd4c31d2bb9226b77aeecc
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-11 18:43:40 +0530

    OptionalOfEmptyNullable
    OptionalOfEmptyNullable

--- java8-lamdas-streams/src/main/java/com/java8/features/optional/OptionalOfEmptyNullable.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/optional/OptionalOfEmptyNullable.java
@@ -1,7 +1,13 @@
 package com.java8.features.optional;
 
+import java.util.Optional;
+
 public class OptionalOfEmptyNullable {
     public static void main(String[] args) {
+        Optional<String> string = Optional.ofNullable("String");
+        System.out.println(string.get());
 
+//        Optional<Object> obj = Optional.of(null);
+//        System.out.println(obj.get());
     }
 }
