8d713be5826e257b6ca6e04e95d197bd28667a72
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-11 17:29:43 +0530

    checkPerformanceResult
    checkPerformanceResult

--- java8-lamdas-streams/src/main/java/com/java8/features/parallelstreams/ParallelStream.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/parallelstreams/ParallelStream.java
@@ -1,5 +1,6 @@
 package com.java8.features.parallelstreams;
 
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class ParallelStream {
@@ -9,5 +10,14 @@ public class ParallelStream {
                 .peek(value -> System.out.println(value))
                 .sum();
         System.out.println(sum);
+    }
+
+    static long checkPerformanceResult(Supplier<Integer> supplier, int noOfTimes) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < noOfTimes; i++) {
+            supplier.get();
+        }
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
     }
 }
