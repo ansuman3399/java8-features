141f7880ec33b6074e7c5a85e82486177668c2cc
Ansuman Mohanty <ansumanm20@gmail.com>
2022-06-12 16:02:47 +0530

    optional or else
    optional or else

--- java8-lamdas-streams/src/main/java/com/java8/features/optional/OptionalOfEmptyNullable.java
+++ java8-lamdas-streams/src/main/java/com/java8/features/optional/OptionalOfEmptyNullable.java
@@ -1,9 +1,15 @@
 package com.java8.features.optional;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public class OptionalOfEmptyNullable {
     public static void main(String[] args) {
+
+        Supplier<String> supplier = () -> {
+            return null;
+        };
+
         Optional<String> string = Optional.ofNullable("String");
         System.out.println(string.get());
 
@@ -12,5 +18,9 @@ public class OptionalOfEmptyNullable {
 
         Optional<Object> empty = Optional.empty();
         System.out.println(empty);
+
+        Optional<String> str = Optional.ofNullable(supplier.get());
+        String value = str.isPresent() ? str.get() : str.orElse("Default");
+        System.out.println("value:" + value);
     }
 }
